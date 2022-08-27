07a49b63928703bf4b52ffb31f50a2e1a5f89930
pecgo-akira <111907631+pecgo-akira@users.noreply.github.com>
2022-08-27 22:25:44 +0700

    style: setup project

--- app/src/main/java/naters/fantasy/pethero/MainActivity.kt
+++ app/src/main/java/naters/fantasy/pethero/MainActivity.kt
@@ -1,19 +1,31 @@
 package naters.fantasy.pethero
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import naters.fantasy.pethero.databinding.ActivityMainBinding
 import naters.fantasy.pethero.presentation.views.LoadingScreenDialogFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setupView()
 
-//        findViewById<Button>(R.id.button).setOnClickListener {
-//            LoadingScreenDialogFragment.getInstance().show(
-//                supportFragmentManager, "Loading"
-//            )
-//        }
+    // Loading Animation
+      /*  findViewById<Button>(R.id.button).setOnClickListener {
+            LoadingScreenDialogFragment.getInstance().show(
+                supportFragmentManager, "Loading"
+            )
+       }*/
+    }
+
+    private fun setupView() {
+        TODO("Not yet implemented")
     }
 }
