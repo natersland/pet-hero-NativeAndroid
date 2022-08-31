e9737f5bfd1bac699fa8f6da77716c52c437f054
Akira Chenyavanij <akirachenyavanij@Akiras-MacBook-Pro.local>
2022-09-01 05:39:12 +0700

    feat: auth feat for challenge later

---
+++ app/src/main/java/naters/fantasy/pethero/AuthenticationScreen.kt
@@ -0,0 +1,63 @@
+package naters.fantasy.pethero
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import naters.fantasy.pethero.databinding.FragmentAuthenticationScreenBinding
+import naters.fantasy.pethero.databinding.FragmentHomeScreenBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class AuthenticationScreen : Fragment() {
+
+    private val binding: FragmentAuthenticationScreenBinding by lazy {
+        FragmentAuthenticationScreenBinding.inflate(layoutInflater)
+    }
+
+   /* // Google Sign-In
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth*/
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+      /*  auth = Firebase.auth
+        //config the Google sign-in
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("547455151198-of1ibdk8vmfktfpnjemo7dk68v290pi7.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+*/
+
+        // Google sign-in button
+        binding.googleSignInButton.setOnClickListener{
+            // begin google sign in
+            Log.d(TAG, "onCreate: begin Google Sign In")
+        }
+
+        return binding.root
+    }
+
+    private fun checkUser() {
+        TODO("Not yet implemented")
+    }
+
+}
--- app/src/main/java/naters/fantasy/pethero/MainActivity.kt
+++ app/src/main/java/naters/fantasy/pethero/MainActivity.kt
@@ -1,23 +1,44 @@
 package naters.fantasy.pethero
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import naters.fantasy.pethero.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    //view binding
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        // Database
         val database = FirebaseDatabase.getInstance("https://pet-hero-a7506-default-rtdb.asia-southeast1.firebasedatabase.app/")
-        val myRef = database.getReference("message")
+        // test database zone
+        val myRef = database.getReference("pet")//.push()
+        val pet1 = Pet("sfafsf",23,213,"dog")
+        myRef.setValue(pet1)
 
-        myRef.setValue("Hello, World!")
+        myRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                val value = p0.value as HashMap<*, *>
+                Log.d("value: ", value.get("name").toString())
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                Log.d("value: ", p0.toString())            }
+
+        })
+
+
 /*
         setupView()
 */
@@ -34,3 +55,6 @@ class MainActivity : AppCompatActivity() {
         TODO("Not yet implemented")
     }
 }
+
+
+data class Pet(var name: String, var petAge: Int, var humanAge: Int, var petType: String)
