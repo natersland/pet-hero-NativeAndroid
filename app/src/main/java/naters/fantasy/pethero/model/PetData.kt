a7cc1ff5f9e5a30ddfa0b4aa07e74fb17d48b30e
Akira Chenyavanij <akirachenyavanij@Akiras-MacBook-Pro.local>
2022-09-08 18:00:50 +0700

    refactor: replace parcelize kotlin

--- app/src/main/java/naters/fantasy/pethero/model/PetData.kt
+++ app/src/main/java/naters/fantasy/pethero/model/PetData.kt
@@ -2,43 +2,14 @@ package naters.fantasy.pethero.model
 
 import android.os.Parcel
 import android.os.Parcelable
-
+import kotlinx.parcelize.Parcelize
+import java.io.Serializable
+@Parcelize
 data class PetData(
     val id: Int?,
-    val petName:String?,
-    val petBirthDate:String?,
-    val petAge:Int?,
-    val humanAge:Int?,
-    val petGender:String?
-):Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString()
-    )
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeValue(id)
-        parcel.writeString(petName)
-        parcel.writeString(petBirthDate)
-        parcel.writeValue(petAge)
-        parcel.writeValue(humanAge)
-        parcel.writeString(petGender)
-    }
-    companion object CREATOR : Parcelable.Creator<PetData> {
-        override fun createFromParcel(parcel: Parcel): PetData {
-            return PetData(parcel)
-        }
-
-        override fun newArray(size: Int): Array<PetData?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    val petName: String?,
+    val petBirthDate: String?,
+    val petAge: Int?,
+    val humanAge: Int?,
+    val petGender: String?
+) : Parcelable
