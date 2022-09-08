1625cc35abee25c56370f64c2cf7f6734dc2ae66
Akira Chenyavanij <akirachenyavanij@Akiras-MacBook-Pro.local>
2022-09-08 17:33:53 +0700

    chore: deleted idea folder

--- app/src/main/java/naters/fantasy/pethero/CreateNewPet.kt
+++ app/src/main/java/naters/fantasy/pethero/CreateNewPet.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import naters.fantasy.pethero.databinding.FragmentCreateNewPetBinding
 import naters.fantasy.pethero.databinding.FragmentHomeScreenBinding
 
@@ -26,16 +29,45 @@ class CreateNewPet : Fragment() {
     ): View? {
 
         val petImage = binding.petPhotoShow
-        val petName = binding.petNameEditText
-        val petGender = when(binding.genderOptions?.checkedRadioButtonId){
+        val petName = binding.petNameEditText.text.toString()
+        val petGender = when (binding.genderOptions?.checkedRadioButtonId) {
             R.id.option_female -> "female"
-            else -> { "male"}
+            else -> {
+                "male"
+            }
         }
         val petBirthDate = binding.editTextDate
-        val aboutPet = binding.aboutPetText
+        val aboutPet = binding.aboutPetText.text.toString()
+        val createNewPetBtn = binding.createNewPetBtn
 
+//        createNewPetBtn.setOnClickListener{
+//            saveToFirebase(petName)
+//        }
 
+        // Create a new user with a first, middle, and last name
+        val user = hashMapOf(
+            "petName" to petName,
+        )
+        val db = Firebase.firestore
+
+// Add a new document with a generated ID
+        db.collection("pets")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                println("DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                println("Error $e")
+            }
         return binding.root
+    }
+
+    private fun saveDataToFirebase(petName: String) {
+        val petName = binding.petNameEditText.text.toString()
+
+        val database =
+            FirebaseDatabase.getInstance("https://pet-hero-a7506-default-rtdb.asia-southeast1.firebasedatabase.app/")
+
     }
 
     // timestamp
